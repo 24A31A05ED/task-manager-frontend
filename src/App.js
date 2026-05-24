e3d693712d999c89835eb74db4c061a3cedef333
Yamini Lakshmi Kantamsetti <yaminilakshmikantamsetti@gmail.com>
2026-05-24 11:33:46 +0530

    add task manager frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import API from "./api";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [title, setTitle] = useState("");
+
+  const fetchTasks = async () => {
+    try {
+      const res = await API.get("/tasks");
+      setTasks(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const addTask = async () => {
+    if (!title) return;
+
+    try {
+      const res = await API.post("/tasks", { title });
+      setTasks([...tasks, res.data]);
+      setTitle("");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: "40px" }}>
+      <h1>Task Manager</h1>
+
+      <input
+        type="text"
+        placeholder="Enter task"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+
+      <button onClick={addTask}>Add Task</button>
+
+      <ul>
+        {tasks.map((task) => (
+          <li key={task._id}>{task.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
